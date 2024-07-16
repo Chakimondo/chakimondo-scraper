8a3b9f40d7057ad3fd13277c917e99e36ad107b8
Paulo Silva Filho <paulosfilho@gmail.com>
2024-07-15 22:44:37 -0300

    First set of tables, to support crawler persistence.

--- migrations/20240716012622_initial_database.js
+++ migrations/20240716012622_initial_database.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('crawler', function (table) {
+      table.bigIncrements('id')
+      table.text('root_path').notNullable().unique()
+      table.string('status', 10).notNullable().defaultTo('idle')
+    })
+    .createTable('link', function (table) {
+      table.bigIncrements('id')
+      table.text('path').notNullable()
+      table.string('status', 10).notNullable().defaultTo('fresh')
+      table.bigInteger('crawler_id').notNullable()
+      table.foreign('crawler_id').references('crawler.id')
+    })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('link').dropTable('crawler')
+}
