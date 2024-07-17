9dee279e8890cdb9c8c7d60bc80064ae60d35123
Paulo Silva Filho <paulosfilho@gmail.com>
2024-07-17 15:01:58 -0300

    Adding yaml support to system, to format initialization file.

--- src/index.js
+++ src/index.js
@@ -7,33 +7,39 @@ const HOME = process.env.HOME
 program
   .name('Celebmeter Scraper')
   .description('Scraper to load newspaper and pages information to evaluate celebrity references.')
-  .version('0.0.0')
+  .version('0.1.0')
 
-program.requiredOption('-u, --url <url>', 'Domain main site')
-program.option('-r, --robots [robots]', 'Use robots.txt file', 'false')
-program.option('-S, --sitemaps [sitemaps]', 'Use sitemaps to crawl', 'false')
-program.option('-d, --depth [depth]', 'Crawling linking search depth from website root.', '9')
-program.option(
-  '-s, --saving-directory <directory>',
-  path.join(HOME, '.kronodynamic-crawl', 'database'),
+program.requiredOption(
+  '-c, --configurations <configuration path>',
+  'System configuration file path',
 )
-program.option('-D, --drop', 'Drop current crawler from database')
-program.option('-c, --clear', 'Clear previous downloaded data from crawling, restarting it.')
-program.option('-R, --restart', 'Restart crawling process, but maintain previously downloaded data')
+// program.requiredOption('-u, --url <url>', 'Domain main site')
+// program.option('-r, --robots [robots]', 'Use robots.txt file', 'false')
+// program.option('-S, --sitemaps [sitemaps]', 'Use sitemaps to crawl', 'false')
+// program.option('-d, --depth [depth]', 'Crawling linking search depth from website root.', '9')
+// program.option(
+//   '-s, --saving-directory <directory>',
+//   path.join(HOME, '.kronodynamic-crawl', 'database'),
+// )
+// program.option('-D, --drop', 'Drop current crawler from database')
+// program.option('-c, --clear', 'Clear previous downloaded data from crawling, restarting it.')
+// program.option('-R, --restart', 'Restart crawling process, but maintain previously downloaded data')
 program.parse()
 
 const O = program.opts()
 
+console.log('Config path: ', O.configurations)
+
 // To use database connection: PostgreSQL - host: localhost, user: kronodynamic, password: krono
 
 // Load file with set of websites to be scraped
-const processor = new DomainProcessor(
-  O.url,
-  O.directory,
-  O.robots !== 'false',
-  O.sitemaps !== 'false',
-  0,
-  parseInt(O.depth),
-)
-// Evaluate all page content:
-processor.evaluate()
+// const processor = new DomainProcessor(
+//   O.url,
+//   O.directory,
+//   O.robots !== 'false',
+//   O.sitemaps !== 'false',
+//   0,
+//   parseInt(O.depth),
+// )
+// // Evaluate all page content:
+// processor.evaluate()
